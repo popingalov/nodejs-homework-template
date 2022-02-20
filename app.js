f166220ea85920f7b117a260c70603cfdc940557
Oleksandr Balitskiy <popingalov@gmail.com>
2022-02-20 03:00:32 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,41 +4,6 @@ const cors = require('cors');
 
 const versionControl = require('./routes/api/versionController');
 
-// const timeOut = [
-//   [9, 10],
-//   [15, 17],
-//   [15, 16],
-//   [14, 16],
-// ];
-
-// const startEnd = [9, 18];
-
-// function workTime(arryTime, time) {
-//   const arry = {};
-
-//   arryTime.forEach(([start, end]) => {
-//     arry[start] = arry[start] > end ? arry[start] : end;
-//   });
-
-//   const od = Object.keys(arry);
-//   const ddo = Object.values(arry);
-
-//   if (od[0] == time[0] && ddo[0] == time[1]) return [];
-
-//   const result = ddo.reduce((acc, el, i) => {
-//     if (el < (od[i + 1] || time[1])) {
-//       acc.push([el, Number(od[i + 1]) || time[1]]);
-//     }
-
-//     return acc;
-//   }, []);
-
-//   if (od[0] > time[0]) result.unshift([time[0], Number(od[0])]);
-
-//   return result;
-// }
-// console.log(workTime(timeOut, startEnd));
-
 const app = express();
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
